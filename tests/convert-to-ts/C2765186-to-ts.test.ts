8bb3c54981a0b98a2188b9e90c841b8ff3112ca4
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-09-13 15:52:25 +0200

    testing

---
+++ tests/convert-to-ts/C2765186-to-ts.test.ts
@@ -0,0 +1,19 @@
+
+import { it, expect } from "bun:test"
+import chipRawEasy from "../assets/C2765186.raweasy.json"
+import { convertBetterEasyToTsx } from "lib/websafe/convert-to-typescript-component"
+import { EasyEdaJsonSchema } from "lib/schemas/easy-eda-json-schema"
+
+it("should convert C2765186 into typescript file", async () => {
+  const betterEasy = EasyEdaJsonSchema.parse(chipRawEasy)
+  const result = await convertBetterEasyToTsx({
+    betterEasy,
+  })
+
+  expect(result).not.toContain("milmm")
+  expect(result).not.toContain("NaNmm")
+
+  // Add more specific assertions here based on the component
+  
+  expect(result).toMatchInlineSnapshot()
+})
